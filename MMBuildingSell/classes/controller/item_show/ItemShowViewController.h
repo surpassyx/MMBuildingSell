d0bb975a78246df35194188f451081621d86d64b
surpassyx <surpassyx@gmail.com>
2013-10-27 10:02:05 +0800

    项目展示页加入视频播放

--- MMBuildingSell/classes/controller/item_show/ItemShowViewController.h
+++ MMBuildingSell/classes/controller/item_show/ItemShowViewController.h
@@ -9,16 +9,23 @@
 #import <UIKit/UIKit.h>
 #import "Palette.h"
 #import <QuartzCore/QuartzCore.h>
+#import "MediaPlayer/MPMoviePlayerController.h"
 
 @interface ItemShowViewController : UIViewController<UINavigationControllerDelegate>
 {
+    //画板
     Palette * myPlalette;
     CGPoint MyBeganpoint;
 	CGPoint MyMovepoint;
     
     UIImageView *imageView;
-    
+    //背景图片
     UIImage * imageBk;
+    
+    //视频控制器
+    MPMoviePlayerController *movie;
+    //播放按钮
+    UIButton* playButton;
 }
 
 @end
--- MMBuildingSell/classes/controller/item_show/ItemShowViewController.m
+++ MMBuildingSell/classes/controller/item_show/ItemShowViewController.m
@@ -23,7 +23,7 @@ static NSMutableArray *colors;
     [super viewDidLoad];
     
     SegmentView *segmentView = [[SegmentView alloc] init];
-    segmentView.titles = @[@"项目展示", @"剖面图", @"全景展示", @"视频展示", @"周边环境", @"房型展示", @"物业展示"];
+    segmentView.titles = @[@"项目展示", @"剖面图", @"全景展示", @"视频展示", @"房型展示", @"物业展示"];
     segmentView.delegate = self;
     self.navigationItem.titleView = segmentView;
 
@@ -38,47 +38,68 @@ static NSMutableArray *colors;
         height = self.view.frame.size.height;
     }
     
+
+    //项目展示图片加载
     imageBk = [UIImage imageNamed:@"6.jpg"];
     imageView = [[UIImageView alloc] initWithImage:imageBk];
     imageView.frame =CGRectMake(0, 0, width, height);
     [self.view addSubview:imageView];
     
+    //画板加载
     myPlalette = [[Palette alloc]initWithFrame:CGRectMake(0, 0, width, height)];
     myPlalette.tag = 1000;
     myPlalette.backgroundColor = [UIColor clearColor];
     [self.view addSubview:myPlalette];
     
+    
+    //播放视频按钮
+    playButton= [[UIButton alloc]initWithFrame:CGRectMake(145, 250, 70, 80)];
+    [playButton addTarget:self action:@selector(PlayMovieAction:) forControlEvents:UIControlEventTouchUpInside];
+    playButton.backgroundColor=[UIColor redColor];
+    [self.view addSubview:playButton];
+    
+    [playButton setHidden:YES];
 
 }
 
 - (void)segmentView:(SegmentView *)segmentView didSelectedSegmentAtIndex:(int)index
 {
-   [myPlalette myalllineclear];
+    [myPlalette myalllineclear];
     [imageView setHidden:NO];
-    NSLog(@"点击了哪个位置---%d", index);
+    [playButton setHidden:YES];
+    if (movie != NULL) {
+        [movie stop];
+    }
+    
     switch (index) {
         case 0:
+            //项目展示背景
             imageBk = [UIImage imageNamed:@"0.jpg"];
             [imageView setImage:imageBk];
             break;
         case 1:
+            //剖面图
             imageBk = [UIImage imageNamed:@"1.jpg"];
             [imageView setImage:imageBk];
             break;
         case 2:
+            //全景展示
             [imageView setHidden:YES];
             break;
         case 3:
-            [imageView setHidden:YES];
+            //视频播放
+            //视频预览图片
+            [playButton setHidden:NO];
+            imageBk = [UIImage imageNamed:@"7.jpg"];
+            [imageView setImage:imageBk];
             break;
         case 4:
-            [imageView setHidden:YES];
-            break;
-        case 5:
+            //房型展示
             imageBk = [UIImage imageNamed:@"5.jpg"];
             [imageView setImage:imageBk];
             break;
-        case 6:
+        case 5:
+            //物业展示
             imageBk = [UIImage imageNamed:@"6.jpg"];
             [imageView setImage:imageBk];
             break;
@@ -131,6 +152,45 @@ static NSMutableArray *colors;
 -(void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
 	//NSLog(@"touches Canelled");
+}
+
+
+-(void)PlayMovieAction:(id)sender{
+    
+    // NSLog(@"PlayMovieAction====");
+    //视频文件路径，此视频已经存入项目包中。属于本地播放
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Movie" ofType:@"m4v"];
+    //视频URL
+    NSURL *url = [NSURL fileURLWithPath:path];
+    //视频播放对象
+    movie = [[MPMoviePlayerController alloc] initWithContentURL:url];
+    movie.controlStyle = MPMovieControlStyleFullscreen;
+    [movie.view setFrame:self.view.bounds];
+    movie.initialPlaybackTime = -1;
+    [self.view addSubview:movie.view];
+    
+//    self.view.userInteractionEnabled = NO;
+    
+    // 注册一个播放结束的通知，当播放结束时，监听到并且做一些处理
+    //播放器自带有播放结束的通知，在此仅仅只需要注册观察者监听通知即可。
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(myMovieFinishedCallback:)
+                                                 name:MPMoviePlayerPlaybackDidFinishNotification
+                                               object:movie];
+    [movie play];
+}
+-(void)myMovieFinishedCallback:(NSNotification*)notify
+{
+    //视频播放对象
+    MPMoviePlayerController* theMovie = [notify object];
+    //销毁播放通知
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:MPMoviePlayerPlaybackDidFinishNotification
+                                                  object:theMovie];
+    [theMovie.view removeFromSuperview];
+//    self.view.userInteractionEnabled = YES;
+
+    // NSLog(@"视频播放完成");
 }
 
 
